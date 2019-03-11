37b1cb5be75f1e95860dd90aeae2e300f0b53416
Canorus <helios7157@gmail.com>
2019-03-11 15:32:46 +0900

    Send status when meed condition

--- bot.py
+++ bot.py
@@ -6,7 +6,9 @@ from random import randint
 import os
 from pytz import timezone
 from credential import retrieve
+from konlpy.tag import Kkma
 
+kkma = Kkma()
 mode = 0
 base = os.path.dirname(os.path.abspath(__file__))
 with open(base+'/cred.json') as fr:
@@ -46,3 +48,32 @@ def upload_media(media_file):
 
 def followback(user_id):
     requests.post(instance+'/api/v1/accounts/'+user_id+'/follow',headers=head)
+
+for l in r_user.iter_lines():
+    dec = l.decode('utf-8')
+    if dec == 'event: notification':
+        mode = 1
+    elif dec == 'event: update':
+        mode = 0
+    elif dec == ':thump':
+        mode = 0
+        continue
+    try:
+        newdec = json.loads(dec.replace('data: ',''))
+        if newdec['account']['bot']:
+            continue
+        try:
+            type = newdec['type']
+        except:
+            pass
+        if ('자','VV') in kkma.pos(newdec['status']):
+            reply_to_id = newdec['status']['id']
+            reply_to_account = newdec['account']['acct']
+            if newdec['account']['display_name']:
+                username = newdec['account']['display_name']
+            else:
+                username = newdec['account']['username']
+            status = '@'+reply_to_account+' 좋은 꿈 꾸세요'
+            mention_to(status, reply_to_id)
+    except:
+        pass
